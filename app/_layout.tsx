75828c40b75e8a00ccc37f050745d7b8242f25d3
Max <jnx477@gmail.com>
2026-03-30 22:50:05 +0500

    arch-49: Добавить компонент прогресс-бара с анимацией и уведомление при level-up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,31 @@
-import { checkAuth } from '@/shared/checkAuth';
-import { router, Stack } from 'expo-router';
-import { useEffect } from 'react';
+// import { checkAuth } from "@/shared/lib/checkAuth";
+import { Stack } from "expo-router";
+// import { useEffect } from "react";
 
 export default function RootLayout() {
   //если пользователь уже авторизован, то перенаправляем его на экран с табами
-  useEffect(() => {
-    const verifyAuth = async () => {
-      const result = await checkAuth(); 
-      if (result) {
-        router.replace('/(tabs)');
-      }
-    };
-    verifyAuth();
-  }, []);
+  // useEffect(() => {
+  //   const verifyAuth = async () => {
+  //     const result = await checkAuth();
+  //     if (result) {
+  //       router.replace("/(tabs)");
+  //     }
+  //   };
+  //   verifyAuth();
+  // }, []);
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)/login" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="leaderboard" options={{ headerShown: true, title: 'Лидерборд' }} />
+      <Stack.Screen
+        name="leaderboard"
+        options={{ headerShown: true, title: "Лидерборд" }}
+      />
+      <Stack.Screen
+        name="progress-test"
+        options={{ headerShown: true, title: "ProgressBar Test" }}
+      />
     </Stack>
   );
 }
--- src/shared/lib/routes.ts
+++ src/shared/lib/routes.ts
@@ -1,20 +1,21 @@
 export const ROUTES = {
   auth: {
-    login: '/(auth)/login' as const,
+    login: "/(auth)/login" as const,
   },
   tabs: {
-    root: '/(tabs)' as const,
-    home: '/(tabs)/' as const,
-    map: '/(tabs)/map' as const,
-    chat: '/(tabs)/chat' as const,
-    events: '/(tabs)/events' as const,
-    profile: '/(tabs)/profile' as const,
+    root: "/(tabs)" as const,
+    home: "/(tabs)/" as const,
+    map: "/(tabs)/map" as const,
+    chat: "/(tabs)/chat" as const,
+    events: "/(tabs)/events" as const,
+    profile: "/(tabs)/profile" as const,
   },
-  leaderboard: '/leaderboard' as const,
-  // Для маршрутов с параметрами 
+  leaderboard: "/leaderboard" as const,
+  progressTest: "/progress-test" as const,
+  // Для маршрутов с параметрами
   dynamic: {
     event: (id: string) => `/(tabs)/events/${id}` as const,
-  }
+  },
 } as const;
 
 export type AppRoutes = typeof ROUTES;
