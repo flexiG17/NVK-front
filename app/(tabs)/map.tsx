79eed86285a9872b6e1250224aec4818d107174c
dmitry <permyakovdm76@gmail.com>
2026-04-10 22:18:29 +0500

    nav-124: Изменение расположения компонентов

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,8 +1,8 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useRef } from 'react';
-import { CustomBottomSheet, CustomBottomSheetRef } from '@/pages/maps/widgets/CustomBottomSheet';
-import { CardScreen } from '@/pages/maps/widgets/CardScreen';
+import { CustomBottomSheet, CustomBottomSheetRef } from '@/pages/maps/bottomSheet/office/widgets/CustomBottomSheet';
+import { CardScreen } from '@/pages/maps/bottomSheet/office/widgets/CardScreen';
 
 export default function MapScreen() {
   const sheetRef = useRef<CustomBottomSheetRef>(null);
