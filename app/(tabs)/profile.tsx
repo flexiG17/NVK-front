133e82f5e45bafa01672e18f0dfa3828ffd399cc
Max <jnx477@gmail.com>
2026-04-24 12:27:21 +0500

    #2 Изменил импорт страницы профиля

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,36 +1,5 @@
-// import { ROUTES } from '@/shared/lib/routes';
-// import { clearAuthSession } from '@/shared/lib/authSession';
-// import { useRouter } from 'expo-router';
-import { StyleSheet, ScrollView } from 'react-native';
-import { ProfileHeader } from '@/pages/profile/widgets/ProfileHeader';
-import { ProfileInfo } from '@/pages/profile/widgets/ProfileInfo';
+import ProfileScreen from '@/pages/profile/ProfileScreen';
 
-export default function ProfileScreen() {
-  // const router = useRouter();
-
-  // const handleLogout = async () => {
-  //   await clearAuthSession();
-  //   router.replace(ROUTES.auth.login);
-  // };
-
-  const userInfo = {
-    fio: 'Иванов Иван Иванович',
-    group: 'РИ-123456',
-    institute: 'ИРИТ-РТФ',
-    role: 'Студент',
-  };
-
-  return (
-    <ScrollView style={styles.container}>
-      <ProfileHeader userName="Имя" level={5} currentPoints={1250} maxPoints={5000} />
-      <ProfileInfo userInfo={userInfo} />
-    </ScrollView>
-  );
+export default function Profile() {
+  return <ProfileScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-});
